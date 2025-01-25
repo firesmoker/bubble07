c51c75de88a03f5bd2f490cebb4220c124acaede
Eylon Keshet <>
2025-01-25 11:28:43 +0200

    more animations and code cleaning

--- scripts/bubble_player.gd
+++ scripts/bubble_player.gd
@@ -9,7 +9,9 @@ const JUMP_VELOCITY = -400.0
 @onready var game: Game = $".."
 var dying: bool = false
 @onready var bubble_sprite: AnimatedSprite2D = $BubbleSprite
-
+@onready var fish_sprite: AnimatedSprite2D = $FishSprite
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+var falling: bool = false
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("shoot_bubble"):
@@ -18,16 +20,30 @@ func _unhandled_input(event: InputEvent) -> void:
 func _ready() -> void:
 	velocity = Vector2(0,0)
 
-func _physics_process(delta: float) -> void:
-	
+func move() -> void:
 	velocity.x = move_toward(velocity.x,0,20)
 	velocity.y = move_toward(velocity.y,-50,20)
 	axis.rotation = seek_mouse()
 	move_and_slide()
+
+func handle_animations() -> void:
 	if velocity.x > 10 or velocity.x < -10:
 		bubble_sprite.play("shoot")
+		fish_sprite.play("shoot")
+		if velocity.x > 0:
+			fish_sprite.flip_h = false
+		else:
+			fish_sprite.flip_h = true
 	else:
 		bubble_sprite.play("default")
+		fish_sprite.play("default")
+
+func _physics_process(delta: float) -> void:
+	if not dying:
+		move()
+		handle_animations()
+	elif falling:
+		fall()
 
 func create_projectile() -> void:
 	var new_projectile := bubble_projectile.instantiate()
@@ -52,9 +68,14 @@ func seek_mouse() -> float:
 
 func get_damage() -> void:
 	if not dying:
+		falling = true
+		collision_shape_2d.disabled = true
+		bubble_sprite.play("death")
+		fish_sprite.play("death")
 		game.update_wins("cat")
 		dying = true
 		print("ouch")
+		await get_tree().create_timer(3).timeout
 		#queue_free()
 		respawn()
 
@@ -62,3 +83,9 @@ func respawn() -> void:
 	if not game.game_over_state:
 		global_position = Vector2(0,0)
 		dying = false
+		falling = false
+		#collision_shape_2d.disabled = false
+
+func fall() -> void:
+	velocity.y = 500
+	move_and_slide()
